37e69fa260d6205cb9f027210ac60bb78a395a33
Amina Khadraoui <amina.khadraoui@student.ehb.be>
2025-02-20 11:37:03 +0100

    warm up 2, 2b, 2c

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import { getRandomScores, getStudentJSON, runTitle } from "./lib.js";
 
 runTitle();
-warmup1();
+warmup2();
+warmup2b();
+warmup2c();
 
 function warmup1() {
 	console.log("Exercise week 1");
@@ -17,6 +19,61 @@ function warmup1() {
 function warmup2() {
 	console.log("Exercise week 2");
 	const scores = getRandomScores(10);
+
+	document.querySelector("#button-2a").addEventListener("click", function () {
+		console.log("click Ok");
+		let html = "<ul>";
+		scores.forEach(function (score) {
+			console.log(score);
+			html += `<li>${score}</li>`;
+		});
+		html += "</ul>";
+		document.querySelector("#content-2").innerHTML = html;
+	});
+}
+function warmup2b() {
+	console.log("Exercise week 2");
+	const scores = getRandomScores(10);
+
+	document.querySelector("#button-2b").addEventListener("click", function () {
+		console.log("click Ok");
+		let html = "<ul>";
+		scores.forEach(function (score) {
+			console.log(score);
+			html += `<li>${score}</li>`;
+			scores.sort(function (a, b) {
+				if (a > b) {
+					return 1;
+				} else {
+					return -1;
+				}
+			});
+		});
+		html += "</ul>";
+		document.querySelector("#content-2").innerHTML = html;
+	});
+}
+
+function warmup2c() {
+	const scores = getRandomScores(10);
+	document.querySelector("#button-2c").addEventListener("click", function () {
+		console.log("click Ok");
+		let html = "<ul>";
+
+		scores.forEach(function (score) {
+			console.log(score);
+			html += `<li>${score}</li>`;
+		});
+		html += "</ul>";
+		document.querySelector("#content-2").innerHTML = html;
+
+		const sum = scores.reduce(function (total, current) {
+			return total + current;
+		});
+		document
+			.querySelector("#content-2")
+			.insertAdjacentHTML("beforeEnd", `<h1>Totaal:${sum}</h1>`);
+	});
 }
 
 function warmup3() {
